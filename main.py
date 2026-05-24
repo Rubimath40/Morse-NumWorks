8344bc0141ef68ddd8ee16e709ca5ad8b73b2bda
Rubimath40 <matcbt10@gmail.com>
2026-05-24 12:51:35 +0200

    Add test section to try ecrypt function

--- main.py
+++ main.py
@@ -5,5 +5,12 @@ import time
 import alphabet
 
 def encrypt(message):
+    message = message.upper()
     for old, new in alphabet.encrypt.items():
         message = message.replace(old, new)
+    return message
+
+
+## Test section
+message = input("What's your message? ")
+print(encrypt(message))
