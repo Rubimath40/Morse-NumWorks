8be3d115a70b86bd64cecdafddb2a75bb62661fe
Rubimath40 <matcbt10@gmail.com>
2026-06-05 19:34:20 +0200

    feat: add recieve function!!!

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ import alphabet
 
 def encrypt(message):
     message = message.upper()
-    for old, new in alphabet.encrypt.items():
+    for old, new in alphabet.encrypt:
         message = message.replace(old, new)
     return message
 
 def decrypt(message):
-    message = message + " "
+    message += " "
     for old, new in alphabet.decrypt:
         message = message.replace(old, new)
     return message
@@ -29,3 +29,36 @@ def send(message, timeUnit=0.2):
         mu.set_led((0,0,0))
         time.sleep(timeUnit)
 
+def recieve(timeUnit=0.2):
+    message = ""
+    isPressed = False
+    startTime = 0
+    lastRelease = None
+
+    while not ion.keydown(ion.KEY_BACKSPACE):
+        if ion.keydown(ion.KEY_OK) and not isPressed:
+            if lastRelease != None:
+                releaseTime = time.monotonic() - lastRelease
+                if  releaseTime < timeUnit * 3:
+                    pass
+                elif    releaseTime < timeUnit * 7:
+                    message += " "
+                else:
+                    message += "  "
+            startTime = time.monotonic()
+            isPressed = True
+
+        if not ion.keydown(ion.KEY_OK) and isPressed:
+            endTime = time.monotonic()
+            clicDuration = endTime - startTime
+
+            if clicDuration < timeUnit * 3:
+                message += "."
+            else:
+                message += "_"
+
+            lastRelease = endTime
+            isPressed = False
+
+    message += " "
+    return message
