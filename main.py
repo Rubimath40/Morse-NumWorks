d7eb94ccdc69e688c56aff39e440b9d8220ecc05
Rubimath40 <matcbt10@gmail.com>
2026-06-11 14:39:19 +0200

    fix: local variable error when selecting receiving message feat: add an indicator in receive to help the user to know when the calculator validated a long or a short laps of time

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import time
 import kandinsky
 
 import alphabet
+
+black = (0,0,0)
+white = (255,255,255)
+
 class encryptionService:
     def encrypt(message):
         message = message.upper()
@@ -18,8 +22,8 @@ class encryptionService:
         return message
 
 class messageService:
-    def send(message, timeUnit=0.2):
-        mu.fill((0,0,0))
+    def send(message, timeUnit=0.3):
+        mu.fill(black)
         for i in message:
             if i == ".":
                 mu.set_led((255,0,0))
@@ -29,23 +33,26 @@ class messageService:
                 time.sleep(timeUnit*3)
             else:
                 time.sleep(timeUnit*3)
-            mu.set_led((0,0,0))
+            mu.set_led(black)
             time.sleep(timeUnit)
 
-    def receive(timeUnit=0.2):
-        mu.fill((0,0,0))
-        kandinsky.draw_string("Press [BACKSPACE] to end message", 0, 204)
+    def receive(timeUnit=0.3):
+        global endTime, startTime
+        mu.fill(black)
+        kandinsky.draw_string("Press [BACKSPACE] to end message", 0, 204, black, white)
         message = ""
         isPressed = False
         startTime = 0
+        endTime = 0
         lastRelease = None
 
         long = timeUnit * 3
         space = timeUnit * 7
 
         while not ion.keydown(ion.KEY_BACKSPACE):
-            kandinsky.draw_string(message, 0, 0)
+            kandinsky.draw_string(message, 0, 0, black, white)
             if ion.keydown(ion.KEY_OK) and not isPressed:
+                kandinsky.fill_rect(0, 186, 20, 18, black)
                 if lastRelease != None:
                     releaseTime = time.monotonic() - lastRelease
                     if releaseTime < long:
@@ -56,6 +63,11 @@ class messageService:
                         message += "  "
                 startTime = time.monotonic()
                 isPressed = True
+            if isPressed == True:
+                if time.monotonic() - startTime < long:
+                    kandinsky.draw_string(".", 0, 186, black, white)
+                else:
+                    kandinsky.draw_string("_", 0, 186, black, white)
 
             if not ion.keydown(ion.KEY_OK) and isPressed:
                 endTime = time.monotonic()
@@ -68,22 +80,30 @@ class messageService:
 
                 lastRelease = endTime
                 isPressed = False
+            if not isPressed:
+
+                if time.monotonic() - endTime < long:
+                    kandinsky.fill_rect(0, 186, 20, 18, black)
+                elif time.monotonic() - endTime < space:
+                    kandinsky.draw_string(" ", 0, 186, black, white)
+                else:
+                    kandinsky.draw_string("  ", 0, 186, black, white)
 
         message += " "
         return message
 
     def liveSend():
-        mu.fill((0,0,0))
-        kandinsky.draw_string("Press [BACKSPACE] to end message", 0, 204)
+        mu.fill(black)
+        kandinsky.draw_string("Press [BACKSPACE] to end message", 0, 204, black, white)
         while not ion.keydown(ion.KEY_BACKSPACE):
             if ion.keydown(ion.KEY_OK):
                 mu.set_led((255,0,0))
             else:
-                mu.set_led((0,0,0))
+                mu.set_led(black)
 
 def chooseMode(listChoices=["1. Send a message", "2. Receive a message"]):
-    kandinsky.fill_rect(0,0,320,222,(0,0,0))
-    kandinsky.draw_string("\n".join(listChoices), 0, 0)
+    kandinsky.fill_rect(0,0,320,222,black)
+    kandinsky.draw_string("\n".join(listChoices), 0, 0, black, white)
 
     while True:
         if ion.keydown(ion.KEY_ONE):
@@ -99,16 +119,16 @@ def chooseMode(listChoices=["1. Send a message", "2. Receive a message"]):
             break
     return selectedMode
 
-def main():
+def main(timeUnit=0.3):
     while True:
         mode = chooseMode()
         if mode == 2:
-            morseMessage = messageService.receive()
+            morseMessage = messageService.receive(timeUnit)
             message = encryptionService.decrypt(morseMessage)
-            mu.fill((0,0,0))
-            kandinsky.draw_string("Press [SHIFT] to exit", 110, 204)
-            kandinsky.draw_string("The message is:", 85, 0)
-            kandinsky.draw_string(message, 0, 20)
+            mu.fill(black)
+            kandinsky.draw_string("Press [SHIFT] to exit", 110, 204, black, white)
+            kandinsky.draw_string("The message is:", 85, 0, black, white)
+            kandinsky.draw_string(message, 0, 20, black, white)
             while not ion.keydown(ion.KEY_SHIFT):
                 pass
         elif mode == 1:
@@ -119,5 +139,6 @@ def main():
             elif sendingMode == 2:
                 message = input("What is your message?\n")
                 morseMessage = encryptionService.encrypt(message)
-                messageService.send(morseMessage)
+                messageService.send(morseMessage, timeUnit)
 
+main()
--- mu.py
+++ mu.py
@@ -4,8 +4,8 @@ It replace many things used in mu by kandinsky
 """
 import kandinsky
 
-def set_led(color=(0,0,0)):
-    kandinsky.fill_rect(15, 15, 290, 192, color)
+def set_led(color):
+    kandinsky.fill_rect(20, 20, 280, 182, color)
 
-def fill(color=(0,0,0)):
+def fill(color):
     kandinsky.fill_rect(0, 0, 320, 222, color)
